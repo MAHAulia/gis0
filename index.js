63587869129ab20fef0c2befc5f10f84d0e6bae4
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2023-12-28 22:57:47 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//js
+// Baca data dengan fetch
+const dataGeoJson = await fetch("./data.json").then((response) =>
+  response.json()
+);
+
+// Generate Body Child
+const tbody = document.getElementById("table-body");
+
+tbody.innerHTML = "";
+
+dataGeoJson.features.forEach((rowData, key) => {
+  const row = document.createElement("tr");
+
+  const no = document.createElement("td");
+  no.textContent = key + 1;
+  row.appendChild(no);
+
+  const nama = document.createElement("td");
+  nama.textContent = rowData.propertie.name;
+  row.appendChild(nama);
+
+  const type = document.createElement("td");
+  type.textContent = rowData.geometry.type;
+  row.appendChild(type);
+
+  const coord = document.createElement("td");
+  coord.textContent = JSON.stringify(rowData.geometry);
+  row.appendChild(coord);
+
+  tbody.appendChild(row);
+});
